64f51d085782212416e32c909632ce18a7c93196
deng <dengdengmo@outlook.com>
2018-03-29 10:47:34 +0800

    try cross origin 2

--- src/js/loadMore.js
+++ src/js/loadMore.js
@@ -65,7 +65,7 @@ LoadMore.prototype = {
     if (this.btn.data('loading')) return
     this.btn.data('loading', true)
     $.ajax({
-      url: 'http://platform.sina.com.cn/slide/album_tech',
+      url: 'https://platform.sina.com.cn/slide/album_tech',
       type: 'get',
       dataType: 'jsonp',
       jsonp: 'jsoncallback',
